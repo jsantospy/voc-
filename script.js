ec45bbbb93e1ffbddc55ea000fe009da0a511073
jsantospy <kpnoflow@gmail.com>
2025-02-12 18:41:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const button = document.getElementById('messageButton');
-const message = document.getElementById('message');
-
-const messages = [
-    "Você é a razão do meu sorriso! 😊",
-    "Meu coração acelora por você❤️",
-    "Meu amor por você é como um aplicativo: sempre atualizado! ❤️",
-    "Você é o chocolate que adoça meu dia! 🍫",
-];
-
-button.addEventListener('click', () => {
-    const randomIndex = Math.floor(Math.random() * messages.length);
-    message.textContent = messages[randomIndex];
-    message.classList.remove('hidden');
-});
+function check() {
+  if ( document.forms[0].elements[0].checked == true && document.forms[0].elements[1].checked == true && document.forms[0].elements[2].checked == true ) {
+    if ( ! $('.wrapper').hasClass('throb')) {
+        // Do things on Nav Close
+        $('.wrapper').addClass('throb');
+    } 
+  } else {
+    if ($('.wrapper').hasClass('throb')) {
+        // Do things on Nav Close
+        $('.wrapper').removeClass('throb');
+    }
+  }
+ }
